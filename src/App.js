1e0fb5c54ac3b3055471d376c52d76a934f9ac43
Nichanun Sawok <nichanunsawok@Nichanuns-MacBook-Air.local>
2023-05-04 14:24:30 +1000

    add button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Nichanun Sawok
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Count: {count}</p>
+      <button onClick={handleClick}>Increment</button>
     </div>
   );
 }
